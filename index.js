2e7c0793cadbac471a3d38415d8d8cfcc5319176
Aidan Middleton <aidanemiddleton@gmail.com>
2020-02-15 01:38:34 +0000

    first commit

--- index.js
+++ index.js
@@ -1,29 +1,55 @@
-let balance = 500.00;
+class Account {
 
-class Withdrawal {
-
-  constructor(amount) {
-    this.amount = amount;
+  constructor(username) {
+    this.username = username;
+    // Have the account balance start at $0 since that makes more sense.
+    this.balance = 500;
   }
 
+}
+
+class Transaction {
+
+  constructor(amount, account) {
+    this.amount  = amount;
+    this.account = account;
+  }
   commit() {
-    balance -= this.amount;
+    this.account.balance += this.value
+    console.log('Balance Updated! your new balance is ', this.account.balance)
+  }
+}
+
+class Deposit extends Transaction {
+
+  get value() {
+    console.log('Preparing to deposit ', this.amount);
+    return this.amount
+  }
+
+}
+
+class Withdrawal extends Transaction {
+
+  get value() {
+    console.log('preparing to withdraw ', this.amount);
+    return -this.amount
   }
 
 }
 
 
 
-
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
-
-t1 = new Withdrawal(50.25);
+const myAccount = new Account("Big Bad John");
+console.log('starting balance: ', myAccount.balance);
+const t1 = new Deposit(666, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
-
-t2 = new Withdrawal(9.99);
+const t2 = new Withdrawal(420, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+
+
+
+
